45a52b4e1289a38fbef6ac5caaed53c00428e62c
Oops <16302010059@fudan.edu.cn>
2018-11-04 12:10:37 +0800

    常亮，包含默认端口信息

---
+++ sdfs/src/main/java/sdfs/Contants.java
@@ -0,0 +1,9 @@
+package sdfs;
+
+public class Contants {
+    public final static int SESSION_TIMEOUT = 5000;
+    public final static int DEFAULT_PORT = 7777;
+    public final static int DEFAULT_DATA_NODE_PORT = 8888;
+    public final static int DEFAULT_NAME_NODE_PORT = 9999;
+
+}
