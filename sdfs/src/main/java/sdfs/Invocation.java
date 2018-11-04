8c6e55f46635726d0b8e15919337518a7d0e9c81
Oops <16302010059@fudan.edu.cn>
2018-11-04 12:11:32 +0800

    客户端请求

---
+++ sdfs/src/main/java/sdfs/Invocation.java
@@ -0,0 +1,43 @@
+package sdfs;
+
+import java.io.Serializable;
+
+/**
+ * 请求数据结构：
+ * 方法名
+ * 参数类型
+ * 参数值
+ */
+public class Invocation implements Serializable {
+    private static final long serialVersionUID = -1908860219241192698L;
+    private String methodName;
+    private Class<?>[] parameterTypes;
+    private Object[] arguments;
+    private transient Stub stub;
+
+    public Invocation(String methodName, Class<?>[] parameterTypes, Object[] arguments) {
+        this.methodName = methodName;
+        this.parameterTypes = parameterTypes;
+        this.arguments = arguments;
+    }
+
+    public void setStub(Stub stub) {
+        this.stub = stub;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public Object[] getArguments() {
+        return arguments;
+    }
+
+    public Stub getStub() {
+        return stub;
+    }
+}
