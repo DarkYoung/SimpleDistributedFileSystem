e94da71d8ce88ef060b828327772ca46e9cd0579
Oops <16302010059@fudan.edu.cn>
2018-11-04 12:15:23 +0800

    url，包含主机名、端口、服务器端提供的服务名

---
+++ sdfs/src/main/java/sdfs/Url.java
@@ -0,0 +1,40 @@
+package sdfs;
+
+import static sdfs.Contants.DEFAULT_PORT;
+
+public class Url {
+    private final String host;
+    private final String service;
+    private volatile int port;
+
+    public Url(String host, String service) {
+        this(host, DEFAULT_PORT, service);
+    }
+
+    public Url(String host, int port, String service) {
+        this.host = host;
+        this.port = port;
+        this.service = service;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    @Override
+    public String toString() {
+        return host + ":" + port + "/" + service;
+    }
+}
