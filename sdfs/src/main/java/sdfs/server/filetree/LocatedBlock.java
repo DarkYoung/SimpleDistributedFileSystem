adcebe9d43c07ed773d0e98f73b4ef173d25b293
Jon Zhang <16302010059@fudan.edu.cn>
2018-11-07 18:56:51 +0800

    添加属性，ip地址、端口号

--- sdfs/src/main/java/sdfs/server/filetree/LocatedBlock.java
+++ sdfs/src/main/java/sdfs/server/filetree/LocatedBlock.java
@@ -4,24 +4,45 @@ package sdfs.server.filetree;
 import java.io.Serializable;
 
 public class LocatedBlock implements Serializable {
-
-    private static final long serialVersionUID = 7904651623629246517L;
     private final int blockNumber;
     private final String host;
+    private final int port;
+
     public LocatedBlock(int blockNumber) {
-        this.host = "localhost";
+        this("", blockNumber);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param host        LocatedBlock可能由不同的DataNode管理，host指不同DataNode对应的ip
+     * @param blockNumber
+     */
+    public LocatedBlock(String host, int blockNumber) {
+        this(host, 0, blockNumber);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param host        LocatedBlock可能由不同的DataNode管理，host指不同DataNode对应的ip
+     * @param port        针对同一主机部署多个服务器（DataNode）的情况，以不同的DataNode监听不同端口来区分服务器
+     * @param blockNumber
+     */
+    public LocatedBlock(String host, int port, int blockNumber) {
+        this.host = host;
+        this.port = port;
         this.blockNumber = blockNumber;
     }
 
-    public LocatedBlock(String host, int blockNumber) {
-        this.host = host;
-        this.blockNumber = blockNumber
-    }
-    
     public String getHost() {
         return host;
     }
-    
+
+    public int getPort() {
+        return port;
+    }
+
     public int getBlockNumber() {
         return blockNumber;
     }
@@ -33,11 +54,11 @@ public class LocatedBlock implements Serializable {
 
         LocatedBlock that = (LocatedBlock) o;
 
-        return host.equals(that.host) && blockNumber == that.blockNumber;
+        return host.equals(that.host) && port == that.port && blockNumber == that.blockNumber;
     }
 
     @Override
     public int hashCode() {
-        return host.hasCode + blockNumber;
+        return (host + port + blockNumber).hashCode();
     }
 }
